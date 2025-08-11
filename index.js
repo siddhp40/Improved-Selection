a3faa3946256ebbb2571850feb7b2ad3122d2e51
Siddh <78278467+siddhp40@users.noreply.github.com>
2025-08-11 15:24:54 -0400

    Update index.js
    Added comments

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+const clearStackHotkeyButton = document.getElementById("clearClipboardStackHotkey");
 let copiedElements = "";
+
+/**
+ * Acess user clipboard and instead of clearing the clipboard stack add it to the end
+ * Note: this does not copy css values
+ */
 const userClipboard = navigator.clipboard;
 document.addEventListener("copy", () => {
     copiedElements += document.getSelection().toString();
     userClipboard.writeText(copiedElements);
 });
+/**
+ * If the user presses a specified hotkey then clear the clipboard
+ */
 document.addEventListener("keydown", (event) => {
     if(event.shiftKey && event.key == 'C'){
         userClipboard.writeText("");
     }
 });
-function newHotkey(){
-    const clearStackHotkeyButton = document.getElementById("clearClipboardStackHotkey");
+clearStackHotkeyButton.addEventListener("click", () =>{
     clearStackHotkeyButton.textContent = 'Recording';
-}
-
+});
