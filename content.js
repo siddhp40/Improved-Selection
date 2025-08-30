2ebb037e3664f07c95bb40b1e3ad333d6cb7869e
Siddh <78278467+siddhp40@users.noreply.github.com>
2025-08-30 02:15:56 -0400

    Update content.js
    Remove needless end line charecters

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 (() =>{
-    const currSelection = document.getSelection().toString();
+    const currSelection = document.getSelection().toString().replace(/(\r\n|\n|\r)/gm, "");
     const currClipboard = navigator.clipboard.readText();
     currClipboard.then((clipText) => navigator.clipboard.writeText(clipText + currSelection));
 })();
